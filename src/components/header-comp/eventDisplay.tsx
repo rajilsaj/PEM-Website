1479eb24bacae16db5768a86a8d3952f6f43e344
RajilSaj <svembe@gmail.com>
2023-03-09 05:37:23 -0500

    WIP

--- src/components/header-comp/eventDisplay.tsx
+++ src/components/header-comp/eventDisplay.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
+import { TfiCalendar, TfiLocationPin, TfiTicket, TfiTime } from 'react-icons/tfi'
 
 export class EventDisplay extends React.Component {
     render() {
         return <div className="container">
             <div className="d-block">
                 <div className="d-block-elt ">
-                <h4 className=""> Date </h4>
+                    <h4 className=""><TfiCalendar/> Date </h4>
+                    <p>Value 1</p>
                 </div>
                 <div className="d-block-elt ">
-                <h4 className="">Heure </h4>
+                <h4 className=""><TfiTime/>Heure </h4>
+                <p>Value 1</p>
                 </div>
                 <div className="d-block-elt ">
-                <h4 className="">Lieu </h4>
+                <h4 className=""><TfiLocationPin/> Lieu </h4>
+                <p>Value 1</p>
                 </div>
                 <div className="d-block-elt ">
-                <h4 className="">Participation </h4>
+                <h4 className=""><TfiTicket/> Participation </h4>
+                <p>Value 1</p>
                 </div>
             </div>
         </div>
